07214297a3412241a9ecd2f4810a2735a7a5d070
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-20 19:31:59 +0200

    [Bonus] Working api info call

--- client/js/main.js
+++ client/js/main.js
@@ -7,12 +7,13 @@ createApp({
       urlDiscs: "http://localhost/php-dischi-json/server/server.php",
       discs: [],
       searchResult: [],
+      showInfo: false,
       loading: false,
     };
   },
   methods: {
     async getDiscs() {
-      console.log("Chiamata per dischi");
+      console.log("Start Disc Call");
       this.loading = true;
       try {
         response = await axios.get(this.urlDiscs);
@@ -25,21 +26,48 @@ createApp({
     },
 
     async searchById(id) {
-      console.log("Ricerca per ID");
+      console.log(`Search Disc ID: ${id}`);
+      this.showInfo = true;
       this.loading = true;
       try {
         const url = `${this.urlDiscs}?id=${id}`;
-        console.log(url);
+        // console.log(url);
         const response = await axios.get(url);
         this.searchResult = response.data;
-        console.log(response);
+        console.log(this.searchResult);
       } catch (error) {
         console.error("Search Disc By ID ERROR:", error);
       } finally {
         this.loading = false;
       }
     },
+
+    closeInfo() {
+      this.showInfo = false;
+    },
+
+    getPlaysNum(plays) {
+      if (plays <= 999999) {
+        return `${plays}`;
+      } else if (plays > 999999 && plays <= 9999999) {
+        return `${(plays / 1000000).toFixed(1)} mln`;
+      } else {
+        return `${Math.floor(plays / 1000000)} mln`;
+      }
+    },
+
+    getDuration(seconds) {
+      const minutes = Math.floor(seconds / 60);
+      const remainingSeconds = seconds % 60;
+
+      // Pad the seconds with a zero if they are less than 10
+      const paddedSeconds =
+        remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
+
+      return `${minutes}:${paddedSeconds}`;
+    },
   },
+  computed: {},
   created() {
     this.getDiscs();
   },
