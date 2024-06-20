68a0715281496f9b1025946ec7d0bedb4713c88c
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-20 14:55:38 +0200

    [UPD] Linked main.js

--- client/index.php
+++ client/index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <link rel="stylesheet" href="./style/main.css" />
-    <title>Dischi</title>
-  </head>
-  <body>
-    <div id="app"></div>
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js"
-      integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    ></script>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <link rel="stylesheet" href="./style/main.css" />
+  <title>Dischi</title>
+</head>
+
+<body>
+  <div id="app">
+    <h1> {{ title }} </h1>
+  </div>
+  <script src="./js/main.js"></script>
+</body>
+
 </html>
---
+++ client/js/main.js
@@ -0,0 +1,9 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      title: "Disc",
+    };
+  },
+}).mount("#app");
